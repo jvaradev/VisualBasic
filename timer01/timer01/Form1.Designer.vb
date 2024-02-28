50ffd784417dbfabbb90507bbc0d8b063b02dfee
jvaradev <javivara_92@hotmail.com>
2024-02-28 09:35:54 +0100

    Ejercicio Timer de 20 a 0 cada segundo

---
+++ timer01/timer01/Form1.Designer.vb
@@ -0,0 +1,60 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Reloj = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Reloj
+        '
+        Me.Reloj.AutoSize = True
+        Me.Reloj.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Reloj.Location = New System.Drawing.Point(111, 102)
+        Me.Reloj.Name = "Reloj"
+        Me.Reloj.Size = New System.Drawing.Size(36, 39)
+        Me.Reloj.TabIndex = 0
+        Me.Reloj.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.Controls.Add(Me.Reloj)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Reloj As System.Windows.Forms.Label
+
+End Class
---
+++ timer01/timer01/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Dim contador As Integer = 20
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If contador >= 0 Then
+            Reloj.Text = contador.ToString()
+            contador -= 1
+        Else
+            Timer1.Stop() ' Detener el temporizador cuando alcanza 0
+        End If
+    End Sub
+
+End Class
