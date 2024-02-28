7eea61b7f6a1341e3d758c5f758ab20e5da5eaa7
jvaradev <javivara_92@hotmail.com>
2024-02-28 18:13:03 +0100

    Ejercicio de escribir y leer archivo de texto

---
+++ escribirArchivoTexto/escribirArchivoTexto/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.BtCrearDoc = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtLeer = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 56)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(287, 145)
+        Me.TextBox1.TabIndex = 0
+        '
+        'BtCrearDoc
+        '
+        Me.BtCrearDoc.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtCrearDoc.Location = New System.Drawing.Point(12, 223)
+        Me.BtCrearDoc.Name = "BtCrearDoc"
+        Me.BtCrearDoc.Size = New System.Drawing.Size(287, 35)
+        Me.BtCrearDoc.TabIndex = 1
+        Me.BtCrearDoc.Text = "CREAR DOCUMENTO"
+        Me.BtCrearDoc.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(272, 20)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Escribe lo que añadir a tu documento"
+        '
+        'BtLeer
+        '
+        Me.BtLeer.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtLeer.Location = New System.Drawing.Point(12, 264)
+        Me.BtLeer.Name = "BtLeer"
+        Me.BtLeer.Size = New System.Drawing.Size(287, 35)
+        Me.BtLeer.TabIndex = 3
+        Me.BtLeer.Text = "LEER DOCUMENTO"
+        Me.BtLeer.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(338, 316)
+        Me.Controls.Add(Me.BtLeer)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtCrearDoc)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents BtCrearDoc As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents BtLeer As System.Windows.Forms.Button
+
+End Class
---
+++ escribirArchivoTexto/escribirArchivoTexto/Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim textoEscribir As String
+    Dim rutaArchivo As String = "C:\Users\javiv\Documents\Visual Studio 2010\Projects\Examen\escribirArchivoTexto\texto.txt"
+
+    Private Sub BtCrearDoc_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtCrearDoc.Click
+        ' Obtener el texto a escribir desde el TextBox
+        textoEscribir = TextBox1.Text
+        ' Utilizar un bloque Try-Catch para manejar excepciones
+        Try
+            ' Crear un StreamWriter y abrir el archivo para escritura
+            Using swEscritor As New StreamWriter(rutaArchivo, True) ' El segundo parámetro indica si se añadirá al final del archivo (True)
+                ' Escribir el contenido en el archivo
+                swEscritor.WriteLine(textoEscribir)
+            End Using
+
+            Label1.Text = "Documento creado exitosamente."
+        Catch ex As Exception
+            Label1.ForeColor = Color.Red
+            Label1.Text = "Error al crear el archivo"
+        End Try
+    End Sub
+
+    Private Sub BtLeer_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtLeer.Click
+        TextBox1.Clear()
+        ' Utilizar un bloque Try-Catch para manejar excepciones
+        Try
+            ' Verificar si el archivo existe antes de intentar leerlo
+            If File.Exists(rutaArchivo) Then
+                ' Crear un StreamReader y abrir el archivo para lectura
+                Using srLector As New StreamReader(rutaArchivo)
+                    ' Leer el contenido completo del archivo y mostrarlo en un TextBox
+                    TextBox1.Text = srLector.ReadToEnd()
+                End Using
+
+                Label1.Text = "Documento leído exitosamente."
+            Else
+                Label1.ForeColor = Color.Red
+                Label1.Text = "El documento no existe."
+            End If
+        Catch ex As Exception
+            Label1.ForeColor = Color.Red
+            Label1.Text = "Error al leer el documento."
+        End Try
+    End Sub
+End Class
