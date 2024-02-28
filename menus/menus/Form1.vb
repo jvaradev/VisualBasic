1cc42cda906a6d789e45f7ad59f2131524891f61
jvaradev <javivara_92@hotmail.com>
2024-02-28 10:07:12 +0100

    Timer Ejercicio Timer de 20 a 0 cada segundo

---
+++ menus/menus/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Dim progresoActual As Integer = 0
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If progresoActual < ProgressBar1.Maximum Then
+            progresoActual += 1
+            ProgressBar1.Value = progresoActual
+        Else
+            Timer1.Stop() ' Detener el temporizador cuando alcanza el máximo
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Button1.Visible = False
+        progresoActual = 0
+        ProgressBar1.Value = progresoActual
+        Timer1.Start() ' Iniciar el temporizador cuando se hace clic en el botón
+    End Sub
+
+End Class
+
