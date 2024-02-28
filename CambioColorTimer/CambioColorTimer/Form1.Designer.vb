e354396b57733e48acfe943c08a37aaaa5fe83a8
jvaradev <javivara_92@hotmail.com>
2024-02-28 18:12:17 +0100

    Ejercicio de cambio de color gradual con Timer

---
+++ CambioColorTimer/CambioColorTimer/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.BtColor = New System.Windows.Forms.Button()
+        Me.Texto1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'BtColor
+        '
+        Me.BtColor.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtColor.Location = New System.Drawing.Point(12, 179)
+        Me.BtColor.Name = "BtColor"
+        Me.BtColor.Size = New System.Drawing.Size(246, 62)
+        Me.BtColor.TabIndex = 0
+        Me.BtColor.Text = "CAMBIA COLOR"
+        Me.BtColor.UseVisualStyleBackColor = True
+        '
+        'Texto1
+        '
+        Me.Texto1.AutoSize = True
+        Me.Texto1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Texto1.Location = New System.Drawing.Point(68, 85)
+        Me.Texto1.Name = "Texto1"
+        Me.Texto1.Size = New System.Drawing.Size(135, 29)
+        Me.Texto1.TabIndex = 1
+        Me.Texto1.Text = "COLORES"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.Controls.Add(Me.Texto1)
+        Me.Controls.Add(Me.BtColor)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents BtColor As System.Windows.Forms.Button
+    Friend WithEvents Texto1 As System.Windows.Forms.Label
+
+End Class
---
+++ CambioColorTimer/CambioColorTimer/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Dim colorInicial As Color = Color.Black
+    Dim colorFinal As Color = Color.Blue
+    Dim paso As Integer = 0
+    Dim cantPasos As Integer = 100
+
+    Private Sub BtColor_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtColor.Click
+        Timer1.Start()
+        BtColor.Visible = False
+        If paso = cantPasos Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles Timer1.Tick
+        If paso < cantPasos Then
+            ' Calcular el color intermedio
+            Dim r As Integer = CInt(colorInicial.R + (colorFinal.R - colorInicial.R) * paso / cantPasos)
+            Dim g As Integer = CInt(colorInicial.G + (colorFinal.G - colorInicial.G) * paso / cantPasos)
+            Dim b As Integer = CInt(colorInicial.B + (colorFinal.B - colorInicial.B) * paso / cantPasos)
+
+            ' Asignar el nuevo color al Texto1
+            Texto1.ForeColor = Color.FromArgb(r, g, b)
+
+            ' Incrementar el paso actual
+            paso += 1
+        Else
+            ' Detener el Timer cuando se alcanza el color final
+            Timer1.Stop()
+            Texto1.ForeColor = Color.Red
+            Texto1.Text = "TERMINADO"
+
+            BtColor.Visible = True
+            BtColor.Text = "CERRAR"
+            BtColor.ForeColor = Color.Red
+        End If
+    End Sub
+
+End Class
